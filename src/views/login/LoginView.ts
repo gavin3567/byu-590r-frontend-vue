49ef20705d10792ef7c8fc0d4563d10383706a16
Gavin Ostheimer <gavinostheimer@mac.lan>
2025-02-15 21:56:19 -0700

    login page code

--- src/views/login/LoginView.ts
+++ src/views/login/LoginView.ts
@@ -1,15 +1,71 @@
-export default {
-    name: 'LoginView',
-    emits: ['authenticate'],
-    data: function () {
-        return {
-            isAuthenticated: false
-        }
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginView',
+  props: {
+    isAuthenticated: {
+      type: Boolean,
+      default: false,
     },
-    methods: {
-        submitLogin() {
-            this.isAuthenticated = true;
-            this.$emit('authenticate', this.isAuthenticated)
-        }
+  },
+  emits: ['authenticate'],
+  data() {
+    return {
+      showLoginForm: false,
+      isValid: false,
+      username: '',
+      password: '',
+      loading: false,
+      resetEmail: '',
+      forgotPasswordDialog: false,
+      hardcodedUsername: 'BYU',
+      hardcodedPassword: 'CosmoCougar1',
+      loginStatus: {
+        show: false,
+        type: '',
+        message: '',
+      },
+      usernameRules: [
+        (v: string) => !!v || 'Username is required',
+        (v: string) => v.length >= 3 || 'Min 3 characters',
+      ],
+      passwordRules: [
+        (v: string) => !!v || 'Password is required',
+        (v: string) => v.length >= 8 || 'Password must be at least 8 characters',
+      ],
     }
-}
+  },
+  methods: {
+    submitLogin() {
+      if (!this.showLoginForm) {
+        this.showLoginForm = true
+        return
+      }
+
+      this.loginStatus.show = true
+
+      if (this.password === this.username) {
+        this.loginStatus.type = 'warning'
+        this.loginStatus.message = 'Your username and password can not be the same!'
+        return
+      }
+
+      if (this.username !== this.hardcodedUsername || this.password !== this.hardcodedPassword) {
+        this.loginStatus.type = 'error'
+        this.loginStatus.message = 'Login Failed! Can not Authenticate!'
+        return
+      }
+
+      // Successful login
+      this.loading = true
+      this.loginStatus.type = 'success'
+      this.loginStatus.message = 'Login Success. Redirecting!'
+
+      setTimeout(() => {
+        this.$emit('authenticate', true)
+        // Navigate to home page
+        this.$router.push('')
+      }, 3000)
+    },
+  },
+})
--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,7 +1,65 @@
 <template>
   <button v-if="!isAuthenticated" @click="submitLogin()">Login Page Placeholder Here</button>
+  <div v-if="showLoginForm && !isAuthenticated" class="login-page">
+    <div class="login-form-container">
+      <v-form v-model="isValid" @submit.prevent="submitLogin">
+        <v-card class="pa-6">
+          <v-alert
+            v-if="loginStatus.show"
+            :type="loginStatus.type"
+            :text="loginStatus.message"
+            class="mb-4"
+          />
+
+          <!-- Form fields in flex column -->
+          <div class="form-fields">
+            <v-text-field v-model="username" label="Username" :rules="usernameRules" required />
+
+            <v-text-field
+              v-model="password"
+              label="Password"
+              type="password"
+              :rules="passwordRules"
+              required
+            />
+
+            <!-- Buttons section with proper gaps -->
+            <div class="button-container">
+              <v-btn color="primary" type="submit" :loading="loading" :disabled="!isValid" block>
+                Login
+              </v-btn>
+
+              <v-btn variant="text" color="primary" @click="forgotPasswordDialog = true" block>
+                Forgot Password?
+              </v-btn>
+            </div>
+          </div>
+        </v-card>
+      </v-form>
+
+      <v-dialog v-model="forgotPasswordDialog" max-width="700px">
+        <v-card>
+          <v-card-title>Reset Password</v-card-title>
+          <v-card-text>
+            <div class="d-flex flex-column align-center justify-center pa-4" style="width: 100%">
+              <v-text-field
+                v-model="resetEmail"
+                label="Email"
+                type="email"
+                class="mb-4"
+                style="min-width: 550px"
+              />
+              <v-btn color="primary" block> Reset Password </v-btn>
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="grey" text @click="forgotPasswordDialog = false"> Close </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+  </div>
 </template>
-<script src="./LoginView.ts" />
-
-
-<style src="./LoginView.scss" />
+<script src="./LoginView.ts"></script>
+<style src="./LoginView.scss"></style>
