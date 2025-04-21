0747577e8794a59d1f7b2670a2990aeee7aa404c
Gavin Ostheimer <gavinostheimer@mac.lan>
2025-04-21 16:13:07 -0600

    home and about updates

--- src/views/about/AboutView.vue
+++ src/views/about/AboutView.vue
@@ -1,4 +1,7 @@
 <template>
-  <h1>Welcome to the Pokemon Card Tracking App!</h1>
-  <p>We strive to give you an amazing user experience as you sort your Pokemon Cards!</p>
+  <h1>About</h1>
+  <p>
+    Here on the Pokemon Card App, we strive to give you an amazing user experience as you sort your
+    Pokemon Cards!
+  </p>
 </template>
